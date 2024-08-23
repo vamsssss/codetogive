20cd290fe3becf927c822312636fbc192e07e092
gavwyh <gav@gavs-MacBook-Air.local>
2024-08-23 21:20:05 +0800

    Implement user controller

---
+++ backend/src/controllers/UserController.ts
@@ -0,0 +1,41 @@
+import { Request, Response } from 'express';
+import IUserService from '../services/IUserService';
+
+export class UserController {
+    constructor(private userService: IUserService) {}
+
+    createUser = async (req: Request, res: Response): Promise<void> => {
+        try {
+            const user = await this.userService.createUser(req.body);
+            res.status(201).send(user);
+        } catch (err) {
+            res.status(500).send(err.message);
+        }
+    };
+
+    updateUser = async (req: Request, res: Response): Promise<void> => {
+        try {
+            const user = await this.userService.updateUser(parseInt(req.params.id, 10), req.body);
+            if (user) {
+                res.send(user); // to change this 
+            } else {
+                res.status(404).send('User not found');
+            }
+        } catch (err) {
+            res.status(500).send(err.message);
+        }
+    };
+
+    deleteUser = async (req: Request, res: Response): Promise<void> => {
+        try {
+            const user = await this.userService.deleteUser(parseInt(req.params.id, 10));
+            if (user) {
+                res.send(user); // to change this
+            } else {
+                res.status(404).send('User not found');
+            }
+        } catch (err) {
+            res.status(500).send(err.message);
+        }
+    };
+}
