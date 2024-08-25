d468554271a1daf9f47ab232c00e882a8b34da97
Alisha <alishawadkar@gmail.com>
2024-08-25 18:23:56 +0800

    Update LandingPage.tsx

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,15 +1,17 @@
-import React from "react";
-import './LandingPage.css'
-import {useNavigate} from "react-router-dom";
+import './LandingPage.css';
+import { useNavigate } from 'react-router-dom';
 
 const LandingPage = () => {
     const navigate = useNavigate();
+
     const handleLogin = () => {
-        navigate('/login')
-    }
+        navigate('/login');
+    };
+
     const handleRegister = () => {
-        navigate('/register')
-    }
+        navigate('/register');
+    };
+
     return (
         <div className="full-container">
             <div className="top-panel">
@@ -21,30 +23,27 @@ const LandingPage = () => {
                 <p className="line-2">with a meal</p>
             </div>
             <div className="image-container">
-
                 <div className="first">
-                    <img src="src/assets/landing-1.png" alt="Image 1"/>
-                    <img src="src/assets/semi-first.png" alt="Image 6"/>
-
+                    <img src="src/assets/landing-1.png" alt="Image 1" />
+                    <img src="src/assets/semi-first.png" alt="Image 6" />
                 </div>
                 <div className="second">
-                    <img src="src/assets/landing-2.png" alt="Image 2"/>
+                    <img src="src/assets/landing-2.png" alt="Image 2" />
                 </div>
                 <div className="third">
-                    <img src="src/assets/landing-3.png" alt="Image 3"/>
+                    <img src="src/assets/landing-3.png" alt="Image 3" />
                 </div>
                 <div className="forth">
-                    <img src="src/assets/landing-4.png" alt="Image 4"/>
+                    <img src="src/assets/landing-4.png" alt="Image 4" />
                 </div>
                 <div className="fifth">
-                    <img src="src/assets/landing-5.png" alt="Image 5"/>
-                    <img src="src/assets/semi-last.png" alt="Image 5"/>
+                    <img src="src/assets/landing-5.png" alt="Image 5" />
+                    <img src="src/assets/semi-last.png" alt="Image 5" />
                 </div>
                 {/* Add more images as needed */}
             </div>
         </div>
     );
 };
-
 
 export default LandingPage;
